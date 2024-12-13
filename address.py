2a687d09c62df8f35ff2c63ca5fc7906dd290b5a
psychopath-Alien <castricky28@gmail.com>
2024-12-13 23:08:01 +0800

    connect to database

---
+++ address.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@127.0.0.1/school'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] == False
+
+db = SQLAlchemy(app)
