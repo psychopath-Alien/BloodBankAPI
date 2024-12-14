95688a6792abafe7c0673c704415084f200e97d2
psychopath-Alien <castricky28@gmail.com>
2024-12-14 20:55:50 +0800

    medicatin table model

---
+++ medication.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost/bloodbank'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class MedicalCondition(db.Model):
+    __tablename__='medications'
+    code = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(100), nullable=True)
+    other_details = db.Column(db.String(100), nullable=True)
+
+    def to_dict(self):
+        return {
+            "code": self.code,
+            "name": self.name,
+            "description": self.description,
+            "other_details": self.other_details
+        }
+    
