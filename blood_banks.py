43cc7d0e666a088123345571432019093842bd89
psychopath-Alien <castricky28@gmail.com>
2024-12-14 11:19:17 +0800

    create model for blood bank table

--- blood_banks.py
+++ blood_banks.py
@@ -7,3 +7,18 @@ app.config['SQLACHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localho
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class BloodBanks(db.Model):
+    __tablename__ = 'address'
+    id = db.Column(db.Integer, primary_key=True)
+    blood_banks_details = db.Column(db.String(100), nullable=True)
+    ADDRESS_id = db.Column(db.Integer, db.ForeignKey('address.ADDRESS_id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id, 
+            "blood_banks_details": self.blood_banks_details,
+            "ADDRESS_id": self.ADDRESS_id
+        }
+    
+        
